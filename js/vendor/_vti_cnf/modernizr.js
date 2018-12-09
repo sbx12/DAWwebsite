f0124aa352c3543122eacbe7a69d49c6ead99d78
sbx12 <sbx018@gmail.com>
2018-12-09 04:32:22 -0500

    Commit 1

---
+++ js/vendor/_vti_cnf/modernizr.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|ET\\Instructor
+vti_modifiedby:SR|REPTARMENACE\\Reptar
+vti_timelastmodified:TR|02 Dec 2015 04:43:32 -0000
+vti_timecreated:TR|17 Sep 2015 20:52:51 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|FL\\ Studio/FLStudio\\ About.html FL\\ Studio/FLStudio\\ FamousUsers.html Ableton\\ Live/AbletonLiveFamousUser.html master.dwt homeempty.html CoverWebPage.html Logic\\ Pro/LogicProAbout.html Ableton\\ Live/AbletonLiveAbout.html Logic\\ Pro/LogicProFamoususers.html
+vti_nexttolasttimemodified:TW|02 Dec 2015 04:42:24 -0000
+vti_cacheddtm:TX|02 Dec 2015 04:42:24 -0000
+vti_filesize:IR|11370
